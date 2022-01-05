0588d6e54126feba2b52e987198a8eaa23d35a7e
Beto Garcia <robertomedeirosgarcia@gmail.com>
2022-01-05 08:40:25 -0300

    SFC <script setup>

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,5 @@ module.exports = {
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/aspect-ratio"),
   ],
 };
